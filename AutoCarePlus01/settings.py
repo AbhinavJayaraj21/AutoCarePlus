4fa08f2b28703f175e752ce15dcd03f9223dc604
Abhinav <abhinavj2114@gmail.com>
2024-05-09 13:01:53 +0530

    change name of folder static and meida

--- AutoCarePlus01/settings.py
+++ AutoCarePlus01/settings.py
@@ -128,15 +128,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'Static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
                     ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
-MEDIA_URl = '/Media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URl = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
